e75d6bafbe3aa1f3bf8007a95eadc4ba22290ae8
Viacheslav Lohvynov <slavacool25@gmail.com>
2025-04-23 14:43:16 +0200

    0.01

---
+++ src/Hero.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero {
+    public String name;
+    public int health;
+   // public List<Card> deck;
+
+    public Hero(String name, int health) {
+        this.name = name;
+        this.health = health;
+      //  this.deck = new ArrayList<>();
+    }
+}
